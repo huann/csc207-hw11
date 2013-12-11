75fd782b82a948a25d7d423a885faded524b1026
huann <huann@grinnell.edu>
2013-12-10 19:18:55 -0600

    as far as we got - 8 hours

--- src/edu/grinnell/csc207/huann/hw11/JSONparser.java
+++ src/edu/grinnell/csc207/huann/hw11/JSONparser.java
@@ -6,7 +6,10 @@ import java.util.ArrayList;
 public class JSONparser {
 	public static ArrayList<NamedValue> parser(String output) {
 		String rest = output;
-		int open = 1;
+		int level = 0;
+		if (output.charAt(0) == '{') {
+			level++;
+		}
 		String valName = "";
 		String valValue = "";
 		Object newval;
@@ -19,7 +22,19 @@ public class JSONparser {
 		NamedValue val = new NamedValue();
 		val.name = valName;
 
-		while (open > 0) {
+		while (level > 0) {
+			int commas = 0;
+			for (int j = i; output.charAt(j) != ']' && level == 1; j++) {
+				if ((output.charAt(j) == '{')
+						|| (output.charAt(j) == '['))
+					level++;
+				else if ((output.charAt(j) == '}')
+						|| (output.charAt(j) == ']'))
+					level--;
+				else if (output.charAt(j) == ',') {
+					commas++;
+				}
+			}
 			if (output.charAt(i) == ' ') {
 				i++;
 			} // if empty space
@@ -34,7 +49,7 @@ public class JSONparser {
 			else if (output.charAt(i) == ':') {
 				i++;
 				if (output.charAt(i) == '\"') {
-					i++; // move past the opening quotation mark
+					i++; // move past the leveling quotation mark
 					while (output.charAt(i) != '\"') {
 						valValue.concat(Character.toString(output.charAt(i)));
 						i++;
@@ -47,48 +62,56 @@ public class JSONparser {
 				else if (output.charAt(i) == '{') {
 					newval = parser(rest);
 					val.value = newval;
-				} // if open curly brace
+				} // if level curly brace
 				else if (output.charAt(i) == '[') {
 					i++;
-					int endpos = output.indexOf(']', i);
+//					int endpos = output.indexOf(']', i);
 					int samelevel = 0;
 					int commacount = 0;
-					while (output.charAt(i) != ']' && samelevel == 0) {
-						if ((output.charAt(i) == '{')
-								|| (output.charAt(i) == '['))
+					for (int k = i; output.charAt(k) != ']' && samelevel == 0; k++) {
+						if ((output.charAt(k) == '{')
+								|| (output.charAt(k) == '['))
 							samelevel++;
-						else if ((output.charAt(i) == '}')
-								|| (output.charAt(i) == ']'))
+						else if ((output.charAt(k) == '}')
+								|| (output.charAt(k) == ']'))
 							samelevel--;
-						else if (output.charAt(i) == ',') {
+						else if (output.charAt(k) == ',') {
 							commacount++;
 						}
-						i++;
 					}
 					Object[] newarray = new Object[commacount + 1];
-					for (int j = 0; j < commacount; j++) {
+					for (int k = 0; k < commacount; k++) {
 						// will break if we have empty array
-						newarray[j] = parser(output.substring(i,
+						newarray[k] = parser(output.substring(i,
 								output.indexOf(',', i) - 1));
+						i = output.indexOf(',', i)+1;
 					} 
 					newarray[commacount]= parser(output.substring(i, output.indexOf(']', i)-1));
-				} // else if open bracket
+				} // else if level bracket
 				else {
-
+					while (Character.isDigit(output.charAt(i))) {
+						valValue.concat(Character.toString(output.charAt(i)));
+						i++;
+					} // while
 				} // else it's a number
 			} // else if colon
-
 			else if (output.charAt(i) == '{') {
 				newval = parser(rest.substring(1));
 				val.value = newval;
 			} //
 
 			else if (output.charAt(i) == ',') {
-
+				for (int l = 0; l < commas; l++) {
+					// will break if we have empty array
+					newList.addAll(parser(output.substring(i,
+							output.indexOf(',', i) - 1)));
+					i = output.indexOf(',', i)+1;
+			}
+				newList.addAll(parser(output.substring(i, output.indexOf('}', i)-1)));
 			} //
 
 			else if (output.charAt(i) == '}') {
-				open = 0;
+				level--;
 			} // else if closing curly brace
 		} // while
 		return newList;
